5ac88047da0d9461a9e110a8e54a1d8f42260e93
中島　茂春 <shigeharu_1978@yahoo.co.jp>
2019-01-26 21:04:22 +0900

    switch文のクラス追加

---
+++ src/main/java/lists/chap03/list03_04/MainSwitch.java
@@ -0,0 +1,20 @@
+package lists.chap03.list03_04;
+public class MainSwitch {
+  public static void main(String[] args) {
+    System.out.println("あなたの運勢を占います");
+    int fortune = 1;    //常に1 
+    switch(fortune) {
+      case 1:
+        System.out.println("大吉");
+        break;
+      case 2:
+        System.out.println("中吉");
+        break;
+      case 3:
+        System.out.println("吉");
+        break;
+      default:
+        System.out.println("凶");
+    }
+  }
+}
